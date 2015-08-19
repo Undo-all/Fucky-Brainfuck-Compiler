57b7a3a7ab60f70e3462060d6afd1017c250547f
Undo-all <undoall2@gmail.com>
2015-08-18 23:25:54 -0400

    removed unused shit

--- bfc.hs
+++ bfc.hs
@@ -6,8 +6,7 @@ data Expr = Add Int
           | Open
           | Close 
           | ScanLeft
-          | ScanRight 
-          | Balanced [(Int, Int)] deriving (Eq, Show)
+          | ScanRight deriving (Eq, Show)
 
 parseGroup :: (Int -> Expr) -> String -> Int -> Char -> Char -> [Expr]
 parseGroup f [] n o c = [f n]
@@ -15,16 +14,6 @@ parseGroup f (x:xs) n o c
     | x == o    = parseGroup f xs (n + 1) o c
     | x == c    = parseGroup f xs (n - 1) o c
     | otherwise = if n == 0 then parse (x:xs) else f n : parse (x:xs)
-
-isBalanced :: String -> Bool
-isBalanced xs = pure xs && foldl movement 0 xs == 0
-  where pure = all (not . (`elem` ".,"))
-        movement acc '>' = acc + 1
-        movement acc '<' = acc - 1
-        movement acc _   = acc 
-
---balance :: String -> Balanced
---balance =  
 
 parse :: String -> [Expr]
 parse []       = []
@@ -38,8 +27,6 @@ parse (',':xs) = Read : parse xs
 parse ('[':'-':']':xs) = Zero : parse xs
 parse ('[':'<':']':xs) = ScanLeft : parse xs
 parse ('[':'>':']':xs) = ScanRight : parse xs
--- Significantly more complex
--- optimization: turn any balanced binary loop into a single iteration
 parse ('[':xs) = Open : parse xs
 parse (']':xs) = Close : parse xs
 parse (_:xs)   = parse xs
